c7ca616fe496ac0a3cc1fb8d7375efac1fde003d
chat2neil <chat2neil@gmail.com>
2023-07-30 13:51:25 +1200

    got the stored proc to table mapping logic to work

---
+++ lib/stored_procedure_to_table_mapper.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+
+class StoredProcedureToTableMapper:
+    """
+    This class knows the structure of the DDL statements DataFrame and and iterates through each row
+    to find the tables that are manipulated by each procedure.
+
+    It creates a new dataframe that has the following columns:
+    table_name, sql_operation, procedure_name
+
+    Code parsing is delegated to the sql_code_parser object.
+    """
+    CACHE_FILE_NAME = './results/tables_to_procs_cache.csv'
+
+    def __init__(self, sql_code_parser, use_cache=True) -> None:
+        self.sql_code_parser = sql_code_parser
+        self.use_cache = use_cache
+
+
+    def _execute_mapping(self, ddl_df):
+        procedures_ds = ddl_df[ddl_df['sql_operation'] == 'CREATE PROCEDURE']
+        procedure_names = procedures_ds['db_object_name'].tolist()
+
+        output_df = pd.DataFrame(columns=['table_name', 'sql_operation', 'procedure_name'])
+        
+        for procedure_name in procedure_names:
+            
+            sql_code = procedures_ds[procedures_ds['db_object_name'] == procedure_name]['sql_code'].values[0]
+            procedure_code = self.sql_code_parser._extract_procedure_declaration_from_code(procedure_name, sql_code)
+            
+            tables = self.sql_code_parser.find_tables_manipulated_by_procedure(procedure_name, procedure_code)
+            
+            for table in tables:
+                new_row = pd.DataFrame([{
+                    'table_name': table['table_name'], 
+                    'sql_operation': table['sql_operation'], 
+                    'procedure_name': procedure_name
+                }])
+                output_df = pd.concat([output_df, new_row], ignore_index=True)
+        
+        return output_df
+
+
+    def map_procedures_to_tables(self, ddl_df):
+        """
+        Iterate through each procedure and find the tables that are manipulated by each procedure.
+        """
+        if self.use_cache and os.path.exists(StoredProcedureToTableMapper.CACHE_FILE_NAME):
+            return pd.read_csv(StoredProcedureToTableMapper.CACHE_FILE_NAME)
+        else:
+            result = self._execute_mapping(ddl_df)
+            result.to_csv(StoredProcedureToTableMapper.CACHE_FILE_NAME, index=False)
+            return result
+    
